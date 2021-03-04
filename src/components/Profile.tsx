5d62cce14517e0a5677a91369113fb056f324660
Keith Sasaki <keith.sasaki5@gmail.com>
2021-03-03 22:21:43 -0300

    Adicionada funcionalidade de completar um desafio, e deixar o level do usuário dinâmico

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
 import styles from '../styles/components/Profile.module.css'
+import { ChallengesContext } from '../contexts/ChallengesContext'
+import { useContext } from 'react'
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext)
+
   return(
     <div className={styles.profileContainer}>
       <img src="https://github.com/keithsasaki.png" alt="profile picture" />
@@ -8,7 +12,7 @@ export function Profile() {
         <strong>Keith Sasaki</strong>
         <p>
           <img src="icons/level.svg" alt="Level"></img>
-          Level 1
+          {level}
         </p>
       </div>
     </div>
